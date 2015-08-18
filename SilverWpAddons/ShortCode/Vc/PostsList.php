5e20f05ef327d2e4fedd8322493bd4cf1df71376
Marcin <marcin.dobroszek@gmail.com>
2015-08-18 10:57:16 +0200

    change - description of PostsList SC field

--- SilverWpAddons/ShortCode/Vc/PostsList.php
+++ SilverWpAddons/ShortCode/Vc/PostsList.php
@@ -78,7 +78,7 @@ if ( ! class_exists( '\SilverWpAddons\ShortCode\PostsList' ) ) {
 						'value' => 'list',
 					),
 					array(
-						'label' => Translate::translate( 'grid (1 column)' ),
+						'label' => Translate::translate( 'grid (1 column) - recommended only if you use Sidebar' ),
 						'value' => 'grid1',
 					),
 					array(
