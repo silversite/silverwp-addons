71d4507d2a9f7af711547e378aaba793bacc216c
Michal Kalkowski <michal@silversite.pl>
2015-09-07 19:14:11 +0200

    Widget Flickr - add form controls description

--- SilverWpAddons/Widget/Flickr.php
+++ SilverWpAddons/Widget/Flickr.php
@@ -57,21 +57,30 @@ if ( ! class_exists( 'SilverWpAddons\Widget\Flickr' ) ) {
 
 			$type = new Select( 'type' );
 			$type->setLabel( Translate::translate( 'Type' ) );
+			$type->setDescription( Translate::translate( 'The type of images from user or group.' ) );
 			$type->addOption( 'user', Translate::translate( 'user' ) );
 			$type->addOption( 'group', Translate::translate( 'group' ) );
 			$this->addControl( $type );
 
 			$flickr_id = new Text( 'flickr_id' );
+			$flickr_id->setDescription(
+				Translate::translate(
+					'Put the flickr ID here, go to <a href="http://goo.gl/PM6rZ" target="_blank">Flickr NSID Lookup</a> if you don\'t know your ID. Example: 71865026@N00'
+				)
+			);
 			$flickr_id->setLabel( Translate::translate( 'Flickr ID' ) );
+			$flickr_id->setDefault( '71865026@N00' );
 			$this->addControl( $flickr_id );
 
 			$count = new Number( 'count' );
 			$count->setLabel( Translate::translate( 'Number of images to show' ) );
 			$count->setDefault( 5 );
+			$count->setDescription( Translate::translate( 'Number of images shown from 1 to 10' ) );
 			$this->addControl( $count );
 
 			$display_method = new Select( 'display' );
 			$display_method->setLabel( Translate::translate( 'Display Method' ) );
+			$display_method->setDescription( Translate::translate( 'Get the image from recent or use random function' ) );
 			$display_method->addOption( 'latest', Translate::translate( 'latest' ) );
 			$display_method->addOption( 'random', Translate::translate( 'random' ) );
 			$this->addControl( $display_method );
